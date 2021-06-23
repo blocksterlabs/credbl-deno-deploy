b6f0819e327476a144adb540d2f9508102e30a14
Ajay Jadhav <jadhavajay@gmail.com>
2021-06-24 00:14:36 +0530

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,21 +1,6 @@
-import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
-import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
-
-function App() {
-  return (
-    <html>
-      <body>
-        <h1>Hello world</h1>
-      </body>
-    </html>
-  );
-}
-
 addEventListener("fetch", (event) => {
-  const html = renderToString(<App />);
-  event.respondWith(
-    new Response(html, {
-      headers: { "content-type": "text/html; charset=utf-8" },
-    }),
-  );
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
 });
